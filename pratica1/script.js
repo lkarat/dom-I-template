f720112cb3ee24905439ce71f0d3117a97ff16e5
Luiz Karat <luiz.karat.silva@gmail.com>
2024-08-07 09:07:28 -0300

    pratica 1

---
+++ pratica1/script.js
@@ -0,0 +1,29 @@
+//prática 1
+console.log(window.document.getElementById("sapo").innerHTML += " pé");
+
+
+
+const evidencias = document.getElementById("poema");
+
+evidencias.innerHTML += " evidências"
+
+console.log(evidencias);
+
+
+
+console.log(document.getElementsByClassName("sabedoria")[0].innerHTML+= " voando");
+
+
+
+
+console.log(document.getElementsByTagName("h1")[0].innerHTML += " picapau");
+
+
+document.getElementById("roberto").innerHTML += " vivi"
+
+
+
+
+
+
+
---
+++ pratica2/script.js
@@ -0,0 +1,4 @@
+
+
+console.log(document.getElementById("nome").value = prompt("escreva seu nome"));
+
